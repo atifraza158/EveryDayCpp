0754dff044f60c2e3bd209c51606b51f6ba75ec5
Atif RaZa <81435791+atifraza158@users.noreply.github.com>
2023-04-12 01:15:24 +0500

    Add files via upload

---
+++ Output.cpp
@@ -0,0 +1,11 @@
+#include<iostream> //here we include our library file called iostream
+using namespace std; // This line just to get standard input/output
+
+
+// Here our main function starts...
+int main() {
+
+    
+    cout<< "Hello World"; //cout command is used to output anything in c++ program...
+    return 0;
+}
