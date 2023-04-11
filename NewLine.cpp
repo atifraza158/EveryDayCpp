6b541896ef5e1f2bbb71f22ba59536040a37889b
Atif RaZa <81435791+atifraza158@users.noreply.github.com>
2023-04-12 01:17:41 +0500

    Add files via upload

---
+++ NewLine.cpp
@@ -0,0 +1,12 @@
+#include <iostream>    //here we include our library file called iostream
+using namespace std;   // This line just to get standard input/output
+
+
+// Here our main function starts...
+int main() {
+
+  cout << "Hello World! \n";
+  cout << "I am learning C++";
+
+  return 0;
+}
